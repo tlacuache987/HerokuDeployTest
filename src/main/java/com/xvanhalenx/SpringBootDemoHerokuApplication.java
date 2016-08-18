a92c82d1d04877152e4f4055e99de68fb2b641f0
Ivan Van Halen <Ivan Van Halen>
2016-08-17 23:59:46 -0500

    first commit

---
+++ src/main/java/com/xvanhalenx/SpringBootDemoHerokuApplication.java
@@ -0,0 +1,12 @@
+package com.xvanhalenx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoHerokuApplication.class, args);
+	}
+}
